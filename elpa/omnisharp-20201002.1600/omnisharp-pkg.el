505ee69f875f45764d25476e078e6e1c080ebbc7
Domse007 <dominikkeller55@gmail.com>
2020-10-31 19:16:09 +0100

    omnisharp

---
+++ elpa/omnisharp-20201002.1600/omnisharp-pkg.el
@@ -0,0 +1,20 @@
+(define-package "omnisharp" "20201002.1600" "Omnicompletion (intellisense) and more for C#"
+  '((emacs "24.4")
+    (flycheck "30")
+    (dash "2.12.0")
+    (auto-complete "1.4")
+    (popup "0.5.1")
+    (csharp-mode "0.8.7")
+    (cl-lib "0.5")
+    (s "1.10.0")
+    (f "0.19.0"))
+  :commit "e26ff8b8d34a247cd4a93be5d62a5f21859b7b57" :keywords
+  '("languages" "csharp" "c#" "ide" "auto-complete" "intellisense")
+  :authors
+  '(("Mika Vilpas and others"))
+  :maintainer
+  '("Mika Vilpas and others")
+  :url "https://github.com/Omnisharp/omnisharp-emacs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
