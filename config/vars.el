d790e113f9e30d9f85964ea147991d75c87236d4
Domse007 <dominikkeller55@gmail.com>
2020-12-24 18:51:41 +0100

    hot fixes

--- config/vars.el
+++ config/vars.el
@@ -1,20 +1,20 @@
 (defvar user-system-name (system-name)
-  "This variable contains the name of the host computer"
+;; This variable contains the name of the host computer
   :string)
 
 (defvar user-system-base-path ""
-  "This variable contains the used path in the current session"
+;; This variable contains the used path in the current session
   :string)
 
 
 (defconst user-ivy-posframe-background-color "#171717"
-  "This avariable describes the posframe background color."
+;; This avariable describes the posframe background color.
   :string)
 
 (defconst user-ivy-posframe-border-color "#3B2441"
-  "This avariable describes the posframe border color."
+;; This avariable describes the posframe border color.
   :string)
 
 (defconst user-ivy-posframe-cursor-color "#51AFEF"
-  "This avariable describes the posframe cursor color."  
+;; This avariable describes the posframe cursor color.
   :string)
