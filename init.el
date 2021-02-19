416534010821a80969c252283f36b4f2eb461034
Domse007 <dominikkeller55@gmail.com>
2021-02-19 20:33:08 +0100

    Moved from seperate repo to branch

---
+++ init.el
@@ -0,0 +1,17 @@
+(org-babel-load-file (concat user-emacs-directory "config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (theme-looper autopair pacmacs zone-nyan centaur-tabs company-web js2-mode emmet-mode company-jedi dracula-theme grandshell-theme cobalt doom-themes elpy neotree atom-one-dark-theme fireplace use-package org-bullets nyan-mode htmlize company)))
+ '(zoom-mode t)
+ '(zoom-size (quote (0.618 . 0.618))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
