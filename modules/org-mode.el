0dfe1e34b17be9291770924319d512df8911dd8a
Domse007 <dominikkeller55@gmail.com>
2021-02-28 15:24:50 +0100

    updated README

--- modules/org-mode.el
+++ modules/org-mode.el
@@ -150,3 +150,31 @@
   :hook (org-mode . org-sticky-header-mode))
 
 (use-package org-tempo)
+
+(use-package snipsearch
+  :ensure t
+  :quelpa (snipsearch
+	   :fetcher github
+	   :repo "domse007/snipsearch")
+  :custom ((snipsearch-list '(("org"
+			       "#+TITLE: %1$s\n#+AUTHOR: %2$s\n#+OPTIONS: toc:t date:nil title:t author:t num:t \\:t\n#+EXPORT_FILE_NAME:\n#+LATEX_CLASS: article\n#+LANGUAGE: de\n#+LATEX_HEADER: \\usepackage[AUTO]{babel}\n#+LATEX: \\setlength\\parindent{0pt}\n\n"
+			       0)
+			      ("eq" "\\[\\]" -2)
+			      ("eqi" "\\(\\)" -2)
+			      ("frac" "\\displaystyle\\frac{}{}" -3)
+			      ("vec" "\\begin{pmatrix}  \\  \\  \\end{pmatrix}" -20)))
+	   (snipsearch-author "Dominik Keller"))
+  :bind ("C-c m" . snipsearch))
+
+(use-package ispell
+  :config
+  (setq-default ispell-program-name "aspell")
+  (ispell-change-dictionary "de" t)  
+  :bind ("C-ö" . ispell-word)
+  :hook ((text-mode . flyspell-mode)
+	 (org-mode . flyspell-mode)
+	 (org-mode . (lambda ()
+		       (interactive)
+		       (save-excursion
+			 (goto-char (word-search-forward "#+LANGUAGE: "))
+			 (ispell-change-dictionary (thing-at-point 'word)))))))
